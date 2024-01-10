88b2e7ce35f4acf3d73aa49823447d331812b401
Bibica Gabriel <bibica327@gmail.com>
2024-01-11 00:07:16 +0200

    .

---
+++ js-introduction/script.js
@@ -0,0 +1,58 @@
+let sesiuneaCurenta = 12;
+let numarCursanti;
+
+sesiuneaCurenta = 13;
+console.log(typeof sesiuneaCurenta);
+
+numarCursanti = 11;
+
+console.log(numarCursanti);
+
+let x = 10;
+let y = 23;
+const variabilaLuiGabi = 3;
+let result = x * y + variabilaLuiGabi * x;
+
+console.log(x);
+
+// x = x + 1;
+x++;
+x++;
+
+console.log(x);
+
+console.log(result);
+
+let numar1 = 44;
+let numar2 = "44 A";
+
+console.log(numar1 == numar2);
+
+console.log(true);
+console.log(false);
+
+console.log(3 - 1); // 2
+console.log(3 + 1); // 4
+console.log("3" + 1); //31
+console.log("3" - 1); //2
+
+let numar3 = 25;
+let numar4 = "25";
+
+console.log(numar3 === numar4);
+
+let numar5 = 123;
+let numar6 = 98;
+
+console.log(numar5 !== numar6);
+
+console.log(0.1 + 0.2 == 0.3);
+console.log(0.1 + 0.2);
+
+console.log(0.5 + 0.1 == 0.6);
+
+console.log(true + true);
+
+console.log(typeof NaN);
+
+console.log(3 > 2 >= 1);
